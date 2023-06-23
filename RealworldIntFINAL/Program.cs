bd8a726ca474d74f230dfa293aa8bbb51e31dfcf
bnair123 <47283308+bnair123@users.noreply.github.com>
2023-06-23 20:20:20 +0200

    Class for JSON parsing (stockPrice) and class for processing. Runs in ASync, gets symbol passed along, deserializes the JSON and returns the JSON as string. We then call the Json deserializer to get the specific Price property (The only property we are looking for and only property that gets returned). Then just returns price. Expects symbol to run. WHEN SAVING STOCK INTO DICTIONARY OR WHICH EVER METHOD, REMEMBER TO CHECK IF US MARKET OR NOT, GIVES WEIRD ERRORS OTHERWISE OR JUST FAILS BC ACC IS US ONLY.

---
+++ RealworldIntFINAL/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RealworldIntFINAL;
+public class Program
+{
+    public static async Task Main()
+    {
+        string symbol = "AAPL"; 
+
+        StockTracker stockTracker = new StockTracker();
+        decimal price = await stockTracker.GetStockPrice(symbol);
+
+        Console.WriteLine($"The current price of {symbol} is: {price}");
+    }
+}
---
+++ RealworldIntFINAL/stockPrice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+namespace RealworldIntFINAL;
+
+public class StockPrice
+{
+    [JsonPropertyName("price")]
+    public string Price { get; set; }
+}
