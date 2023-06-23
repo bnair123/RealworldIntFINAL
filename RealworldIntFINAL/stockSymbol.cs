22accb5a6567c95ec78b226f46f9d521c5020385
bnair123 <47283308+bnair123@users.noreply.github.com>
2023-06-23 21:00:55 +0200

    Creates a list of all US equaties to validate user input against and also make the system more modular by giving the option of using multiple APIs if necessary while guaranteeing function with our main provider

---
+++ RealworldIntFINAL/stockSymbol.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Text.Json.Serialization;
+namespace RealworldIntFINAL;
+public class StockSymbol
+{
+    [JsonPropertyName("symbol")]
+    public string Symbol { get; set; }
+}
