1d69b3a3f38ee0b1fdcc28ba3df42e56c2712ee8
bnair123 <47283308+bnair123@users.noreply.github.com>
2023-06-24 22:18:21 +0200

    Seperated 2 classes

--- RealworldIntFINAL/XMLSerializer.cs
+++ RealworldIntFINAL/XMLSerializer.cs
@@ -6,11 +6,6 @@ using System.Xml.Serialization;
 namespace RealworldIntFINAL;
 // Main 2 Guides: https://stackoverflow.com/questions/3671259/how-to-xml-serialize-a-dictionary MAIN: http://web.archive.org/web/20100703052446/http://blogs.msdn.com/b/psheill/archive/2005/04/09/406823.aspx
 
-public class StockDictionaryEntry
-{
-    public string Symbol { get; set; }
-    public decimal Price { get; set; }
-}
 public class StockDictionarySerializer
 {
     public void Serialize(TextWriter writer, StockDictionary dictionary)
