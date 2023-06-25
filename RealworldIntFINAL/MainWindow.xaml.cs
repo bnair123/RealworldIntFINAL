10213cb78c15230b07d6c239deb6b21fb06e7c15
Kalduna <gujabidzematt@gmail.com>
2023-06-25 02:22:24 +0200

    Added MessageBox es

--- RealworldIntFINAL/MainWindow.xaml.cs
+++ RealworldIntFINAL/MainWindow.xaml.cs
@@ -31,17 +31,17 @@ namespace RealworldIntFINAL
 
         private void AddStockClicked(object sender, RoutedEventArgs e)//Add Stock Is Clicked 
         {
-
+            MessageBox.Show("Adding stock LMAO + UR MUM");
         }
 
         private void RemoveStockClicked(object sender, RoutedEventArgs e)//Remove Stock Is Clicked
         {
-
+            MessageBox.Show("Removing stock LMAO + UR MUM");
         }
 
         private void UserInputIsBadClicked(object sender, RoutedEventArgs e)//Someone Needs True Guidance In Life
         {
-
+            MessageBox.Show($"I don't remmember this language but I will teach it to you.");
         }
     }
 }
